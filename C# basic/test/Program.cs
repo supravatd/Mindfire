2d81f39e95a1d96814d4af9dcf7f19ed1aeca08f
supravatd <dwarisupravat@gmail.com>
2024-01-23 00:05:33 +0530

    C# basics

---
+++ C# basic/test/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.WriteLine("I am Learning C#");
+            //Console.WriteLine("It is awesome!");
+            //Console.WriteLine(3 + 3);
+            //Console.Write("Hello World! ");
+            //Console.Write("I will print on the same line.");
+
+            //Numbers
+            //int i = 10;
+            //Console.WriteLine(i);
+            //int j = int.Parse(Console.ReadLine());
+            //Console.WriteLine(j);
+
+            //short s = 100;
+            //long l = 10000L;
+            //uint ui = 12;
+
+            //int intNum = (int)l;
+            //short shortNum = (short)i;
+
+            //// Decimal numbers
+            //float f = 10.5f;
+            //double d = 123.99;
+            //decimal dd = 345.80m;
+
+            //float floatNew = (float)dd;
+            //decimal decimalNew = (decimal)f;
+            //decimal decimalNew2 = (decimal)d;
+
+
+
+
+
+            ////Strings
+            //char ch;
+            //ch = 'C';
+            //Console.WriteLine(ch);
+
+
+            //string str;
+            //Console.WriteLine("Enter your name: ");
+            //str = Console.ReadLine();
+            //Console.WriteLine("Hello, " + str);
+
+            //string str2;
+            //Console.Write("Enter your name: ");
+            //str2 = Console.ReadLine();
+            //Console.WriteLine($"Hello, {str2}");
+
+            ////Boolean
+            //bool b;
+            //b = true;
+
+            //Console.WriteLine("Before assignment");
+            //Console.WriteLine(b);
+
+            //b = bool.Parse(Console.ReadLine());
+            //Console.WriteLine("After assignment");
+            //Console.WriteLine(b);
+
+            ////Arrays
+            //string sun = "sunday";
+            //string mon = "monday";
+
+            //string[] days = new string[]
+            //    {
+            //        "Sunday",
+            //        "Monday",
+            //        "Tuesday",
+            //        "Wedneday",
+            //        "Thursday",
+            //        "Friday",
+            //        "Saturday"
+            //    };
+
+            //Console.WriteLine(days[0]);
+            //Console.WriteLine(days[1]);
+            //Console.WriteLine(days[2]);
+
+            //string[] daysNew;
+            //daysNew = new string[7];
+            //daysNew[0] = "Sunday";
+            //daysNew[1] = "Monday";
+            //daysNew[2] = "Tuesday";
+            //Console.Write(daysNew[0]);
+            //Console.Write(daysNew[1]);
+            //Console.Write(daysNew[2]);
+
+            //int length = daysNew.Length;
+            //Console.WriteLine(length);
+
+
+            ////Assignment Operator
+            //int a;
+
+            //a = 10;
+            //a += 5;
+            //Console.WriteLine($"a += 5 : {a}");
+            //a -= 5;
+            //Console.WriteLine($"a -= 5 : {a}");
+            //a *= 5;
+            //Console.WriteLine($"a *= 5 : {a}");
+            //a /= 5;
+            //Console.WriteLine($"a /= 5 : {a}");
+
+
+            //try
+            //{
+            //    int[] myNumbers = { 1, 2, 3 };
+            //    Console.WriteLine(myNumbers[10]);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            string s1 = "Supravat";
+            File.WriteAllText("name.txt", s1);
+
+            string s2 = File.ReadAllText("name.txt");
+            Console.WriteLine(s2);
+
+            Console.ReadLine();
+        }
+    }
+}
