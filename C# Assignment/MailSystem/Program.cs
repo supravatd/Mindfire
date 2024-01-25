f5ecdb5ee9dd9960745ceecbc86bb79903912c92
supravatd <dwarisupravat@gmail.com>
2024-01-26 00:30:56 +0530

    Mail System

---
+++ C# Assignment/MailSystem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+
+namespace MailSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("6638801af2fb3f", "5698daf9b86096"),
+                EnableSsl = true
+            };
+            MailMessage message = new MailMessage("dwarisupravat@gmail.com", "supravatdwari@gmail.com")
+            {
+                Subject = "Test Mail",
+                IsBodyHtml = true,
+                Body = "<h1>Hello!!! from Supravat</h1>"
+            };
+            try
+            {
+                client.Send(message);
+                Console.WriteLine("Sent");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: "+ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
