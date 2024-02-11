ec994343fd5b3add6f2edbd3376b83ddffa54879
supravatd <dwarisupravat@gmail.com>
2024-02-11 14:04:01 +0530

    Global asax

--- AspNetPractice/Global.asax.cs
+++ AspNetPractice/Global.asax.cs
@@ -11,11 +11,91 @@ namespace AspNetPractice
 {
     public class Global : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        protected void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            System.Diagnostics.Trace.WriteLine("Application_Start");
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_End");
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_Error");
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Session_Start");
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Session_End");
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_BeginRequest");
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_EndRequest");
+        }
+
+        protected void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_PreRequestHandlerExecute");
+        }
+
+        protected void Application_PostRequestHandlerExecute(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_PostRequestHandlerExecute");
+        }
+
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_PreSendRequestHeaders");
+        }
+
+        protected void Application_PreSendRequestContent(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_PreSendRequestContent");
+        }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_AcquireRequestState");
+        }
+
+        protected void Application_ReleaseRequestState(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_ReleaseRequestState");
+        }
+
+        protected void Application_ResolveRequestCache(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_ResolveRequestCache");
+        }
+
+        protected void Application_UpdateRequestCache(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_UpdateRequestCache");
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_AuthenticateRequest");
+        }
+
+        protected void Application_AuthorizeRequest(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Application_AuthorizeRequest");
         }
     }
 }
--- AspNetPractice/LifeCycle.aspx.cs
+++ AspNetPractice/LifeCycle.aspx.cs
@@ -13,25 +13,25 @@ namespace AspNetPractice
         protected void Page_PreInit(object sender, EventArgs e)
         {
             // Code for PreInit event
-            Response.Write("PreInit event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("PreInit event fired.<br/>");
         }
 
         protected void Page_Init(object sender, EventArgs e)
         {
             // Code for Init event
-            Response.Write("Init event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("Init event fired.<br/>");
         }
 
         protected void Page_InitComplete(object sender, EventArgs e)
         {
             // Code for InitComplete event
-            Response.Write("InitComplete event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("InitComplete event fired.<br/>");
         }
 
         protected void Page_PreLoad(object sender, EventArgs e)
         {
             // Code for PreLoad event
-            Response.Write("PreLoad event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("PreLoad event fired.<br/>");
         }
 
         protected void Page_Load(object sender, EventArgs e)
@@ -42,44 +42,44 @@ namespace AspNetPractice
         protected void Button2_Click(object sender, EventArgs e)
         {
             // Code to handle Button control's Click event
-            Response.Write("Button Click event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("Button Click event fired.<br/>");
         }
 
         protected void Page_LoadComplete(object sender, EventArgs e)
         {
             // Code for LoadComplete event
-            Response.Write("LoadComplete event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("LoadComplete event fired.<br/>");
         }
 
         protected void Page_PreRender(object sender, EventArgs e)
         {
             // Code for PreRender event
-            Response.Write("PreRender event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("PreRender event fired.<br/>");
         }
 
         protected void Page_PreRenderComplete(object sender, EventArgs e)
         {
             // Code for PreRenderComplete event
-            Response.Write("PreRenderComplete event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("PreRenderComplete event fired.<br/>");
         }
 
         protected void Page_SaveStateComplete(object sender, EventArgs e)
         {
             // Code for SaveStateComplete event
-            Response.Write("SaveStateComplete event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("SaveStateComplete event fired.<br/>");
         }
 
         protected override void Render(System.Web.UI.HtmlTextWriter writer)
         {
             // Code for Render event
-            Response.Write("Render event fired.<br/>");
+            System.Diagnostics.Trace.WriteLine("Render event fired.<br/>");
             base.Render(writer);
         }
 
         protected void Page_Unload(object sender, EventArgs e)
         {
             // Code for Unload event
-            // Note: Response.Write() not recommended here as the response stream is closed
+            // Note: System.Diagnostics.Trace.WriteLine() not recommended here as the response stream is closed
         }
 
         //Sequence:
